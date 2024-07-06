b305f6bb2cfde69dd7e87b098b645d1a20dedfcd
Zombie1995 <egorkerber1995@mail.ru>
2024-07-07 01:51:51 +0700

    vite base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/to-do-app/",
   plugins: [react(), tsconfigPaths(), svgr()],
   optimizeDeps: {
     include: ["reflect-metadata"],
